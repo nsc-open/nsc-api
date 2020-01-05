1acb0bcccaef15d0db99cef7c676874b4806b6ec
Admin <ygjack414@hotmail.com>
2020-01-05 15:24:35 +0800

    update

--- src/index.js
+++ src/index.js
@@ -7,64 +7,78 @@
     root.nscAPI = factory() // Browser globals
   }
 })(this, function () {
-  function factory () {
-    var _version = {} // version.group.name
+  var _version = {} // version.group.name
+  var _defs = {}
 
-    function _parseExp (exp, format) {
-      var items = exp.split('.')
-      if (format === 'array') {
-        return items
-      } else {
-        return {
-          version: items[0],
-          group: items[1],
-          name: items[2]
-        }
+  function _parseExp (exp, format) {
+    var items = exp.split('.')
+    if (format === 'object') {
+      return {
+        version: items[0],
+        group: items[1],
+        name: items[2]
       }
+    } else {
+      return items
     }
-
-    function _get (exp, obj) {
-      
-
-    }
-
-    function _set (exp, obj, val) {
-
-    }
-
-    function define (exp, def) {
-      if (_get(exp)) {
-        console.warn('api of expression[' + exp + '] will be overrided')
-      }
-      _set(exp, def)
-    }
-    function config () {
-
-    }
-    function intercept () {
-
-    }
-    function call (exp, ...args) {
-      var def = _get(exp)
-      if (!def) {
-        throw new Error('api of expression[' + exp + '] does not exist')
-      }
-      if (typeof def === 'function') {
-
-      } else {
-        return def(args)
-      }
-    }
-
-    
-
-    var nscAPI = call
-    nscAPI.define = define
-    nscAPI.config = config
-    nscAPI.intercept = intercept
-    nscAPI.call = call
-
-    return nscAPI
   }
-  return factory
+
+  function _set (path, obj, val) {
+    if (path.length === 0) {
+      return
+    } else if (path.length === 1) {
+      obj[path[0]] = val
+    } else {
+      var attr = path[0]
+      if (!obj[attr]) {
+        obj[attr] = {}
+      }
+      _set(path.slice(1), obj[attr], val)
+    }
+  }
+
+  function define (exp, def) {
+    if (_defs[exp]) {
+      console.warn('api of expression[' + exp + '] will be overrided')
+    }
+
+    _set(_parseExp(exp), _version, def)
+    _defs[exp] = def
+  }
+
+  function call (/* exp, ...args */) {
+    var exp = arguments[0]
+    var args = Array.prototype.slice.call(arguments, 1)
+    var def = _defs[exp]
+
+    if (!def) {
+      throw new Error('api of expression[' + exp + '] does not exist')
+    }
+
+    if (typeof def === 'function') {
+      var parsedExp = _parseExp(exp, 'object')
+      var ctx = { expression: exp, parsedExpression: parsedExp }
+      return def.apply(ctx, args)
+    } else {
+      return axios(def)
+    }
+  }
+
+  function config () {
+
+  }
+
+  function intercept () {
+
+  }
+
+  var nscAPI = call
+  nscAPI.define = define
+  nscAPI.config = config
+  nscAPI.intercept = intercept
+  nscAPI.call = call
+  nscAPI._defs = _defs
+  nscAPI._version = _version
+
+  return nscAPI
 })
